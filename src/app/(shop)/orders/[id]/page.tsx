084ae16b6e8db5190bf1821b64127232bd8d31f0
alfonsoj-entwickler <77501021+alfonsoj-entwickler@users.noreply.github.com>
2024-07-05 17:06:44 +0200

    [TASK] created pages orders

---
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -0,0 +1,9 @@
+ 
+
+export default function Order() {
+  return (
+    <div>
+      <h1>Order ID</h1>
+    </div>
+  );
+}
---
+++ src/app/(shop)/orders/page.tsx
@@ -0,0 +1,9 @@
+ 
+
+export default function Orders() {
+  return (
+    <div>
+      <h1>List of yours Orders</h1>
+    </div>
+  );
+}
