206ede1afa4c2d7e204afdd22c463d1c57fd9f57
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-07-16 21:55:41 +0200

    [TASK] added a function to open menu

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import { titleFont } from "@/config/fonts";
+import { usedUIStore } from "@/store";
 import Link from "next/link";
 import { IoCartOutline, IoSearchOutline } from "react-icons/io5";
 
 export const TopMenu = () => {
+  const openMenu = usedUIStore((state) => state.openSideMenu);
+
   return (
     <nav className="flex justify-between items-center w-full px-5">
       {/* Logo */}
@@ -16,9 +21,24 @@ export const TopMenu = () => {
       </div>
       {/* Center Menu */}
       <div className="hidden sm:flex gap-2">
-        <Link className="p-2 rounded-md transition-all hover:bg-gray-200" href="/category/ps">PS</Link>
-        <Link className="p-2 rounded-md transition-all hover:bg-gray-200" href="/category/xbox">XBOX</Link>
-        <Link className="p-2 rounded-md transition-all hover:bg-gray-200" href="/category/nintendo">SWITCH</Link>
+        <Link
+          className="p-2 rounded-md transition-all hover:bg-gray-200"
+          href="/category/ps"
+        >
+          PS
+        </Link>
+        <Link
+          className="p-2 rounded-md transition-all hover:bg-gray-200"
+          href="/category/xbox"
+        >
+          XBOX
+        </Link>
+        <Link
+          className="p-2 rounded-md transition-all hover:bg-gray-200"
+          href="/category/nintendo"
+        >
+          SWITCH
+        </Link>
       </div>
 
       {/* Search */}
@@ -28,11 +48,18 @@ export const TopMenu = () => {
         </Link>
         <Link href="/cart">
           <div className="relative">
-            <span className="absolute -top-2 -right-2 px-1 rounded-full text-xs font-bold bg-blue-800 text-white">3</span>
+            <span className="absolute -top-2 -right-2 px-1 rounded-full text-xs font-bold bg-blue-800 text-white">
+              3
+            </span>
             <IoCartOutline className="w-5 h-5" />
           </div>
         </Link>
-        <button type="button" className="m-2 p-2 rounded-md transition-all hover:bg-gray-200" aria-label="button to menu">
+        <button
+          type="button"
+          className="m-2 p-2 rounded-md transition-all hover:bg-gray-200"
+          aria-label="button to menu"
+          onClick={() => openMenu()}
+        >
           Menu
         </button>
       </div>
