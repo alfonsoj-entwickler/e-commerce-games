817f055b7b648e61f2be6824106ebf87a6538872
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-07-16 21:56:46 +0200

    [TASK] update paths

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,15 @@
- 
+import { GameGrid, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+
+const games = initialData.products;
 
 export default function Shop() {
   return (
-    <div>
-      <h1>Shop</h1>
-    </div>
+    <>
+      <Title title="Shop" subtitle="All games" className="mb-2" />
+      <GameGrid 
+        games={games}
+      />
+    </>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,6 @@
-export * from './ui/top-menu/TopMenu';
+export * from "./games/game-grid/GameGrid";
+export * from "./games/game-grid/GameGridItem";
+export * from "./ui/sidebar/Sidebar";
+export * from "./ui/top-menu/TopMenu";
+export * from "./ui/not-found/PageNotFound";
+export * from "./ui/title/Title";
