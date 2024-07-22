fca188164cb0e2124f13e34a70b21dc1f0cda2aa
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-07-22 18:05:25 +0200

    [TASK] created page category

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,19 +1,25 @@
+import { GameGrid, Title } from "@/components";
 import { notFound } from "next/navigation";
+import { initialData, ValidHardwares } from "@/seed/seed";
+
+const games = initialData.products;
 
 interface Props {
   params: {
-    id: string;
+    id: ValidHardwares;
   };
 }
 
 export default function Category({ params }: Props) {
   const { id } = params;
-  if (id === "nintendo") {
-    notFound();
-  }
+  //if (id === "nintendo") {
+  //  notFound();
+  //}
+
   return (
-    <div>
-      <h1>category {id}</h1>
-    </div>
+    <>
+      <Title title={id.toUpperCase()} subtitle="Games" className="mb-2" />
+      <GameGrid games={games.filter((item) => (item.hardware === id))} />
+    </>
   );
 }
