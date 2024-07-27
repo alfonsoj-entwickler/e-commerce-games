ae262dfe273d367a203074a310d5e6b0c16bd14d
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-07-27 10:21:08 +0200

    [TASK] created a size selector

---
+++ src/components/game/size-selector/SizeSelector.tsx
@@ -0,0 +1,34 @@
+import type { ValidSizes } from "@/interfaces";
+import clsx from "clsx";
+
+interface Props {
+  selectedSize: ValidSizes;
+  availableSizes: ValidSizes[];
+}
+
+export const SizeSelector = ({ selectedSize, availableSizes }: Props) => {
+  return (
+    <div className="my-5">
+      <h3 className="font-bold mb-4">Hardware available</h3>
+      <div className="flex">
+        {availableSizes.length > 0 &&
+          availableSizes.map((size) => (
+            <button
+              type="button"
+              key={size}
+              className={
+                clsx(
+                    "mx-2 hover:underline text-lg",
+                    {
+                        'underline': size === selectedSize
+                    }
+                )
+              }
+            >
+              {size}
+            </button>
+          ))}
+      </div>
+    </div>
+  );
+};
