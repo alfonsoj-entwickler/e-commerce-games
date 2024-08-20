e5647f6fc1d1b82d05d235bfb0c1957a02ffe9d6
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-08-20 19:27:50 +0200

    [TASK] created a new interface

--- src/interfaces/game.interface.ts
+++ src/interfaces/game.interface.ts
@@ -12,6 +12,16 @@ export interface Game {
     hardware: 'xbox'|'playstation'|'nintendo'|'all'
 }
 
+export interface CartGame {
+    id: string;
+    slug: string;
+    title: string;
+    price: number;
+    quantity: number;
+    size: ValidSizes;
+    image: string;
+}
+
 export type ValidSizes = 'XS'|'S'|'M'|'L'|'XL'|'XXL'|'XXXL';
 export type ValidTypes = 'shirts'|'pants'|'hoodies'|'hats';
 export type ValidHardware = 'XBOX'|'X360'|'XONE'|'XSeries'|'PS2'|'PS3'|'PS4'|'PS5'|'WII'|'WIIU'|'SWITCH';
