f83347e0bace0f060f6fbfc957a589328de3efa3
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-08-23 19:11:14 +0200

    [TASK] updated quantity selectot

--- src/components/game/quantity-selector/QuantitySelector.tsx
+++ src/components/game/quantity-selector/QuantitySelector.tsx
@@ -1,27 +1,29 @@
 "use client";
-import { useState } from "react";
+
 import { IoAddCircleOutline, IoRemoveCircleOutline } from "react-icons/io5";
 
 interface Props {
   quantity?: number;
+  onQuantityChanged: (quantity: number) => void;
 }
 
-export const QuantitySelector = ({ quantity = 1 }: Props) => {
-  const [count, setCount] = useState(quantity);
-
+export const QuantitySelector = ({
+  quantity = 1,
+  onQuantityChanged,
+}: Props) => {
   return (
     <div className="flex items-center">
       <button type="button">
         <IoRemoveCircleOutline
           size={30}
-          onClick={(e) => setCount(count == 1 ? 1 : count - 1)}
+          onClick={(e) => onQuantityChanged(quantity == 1 ? 1 : quantity - 1)}
         />
       </button>
-      <span className="w-20 mx-3 px-5 bg-gray-100 text-center">{count}</span>
+      <span className="w-20 mx-3 px-5 bg-gray-100 text-center">{quantity}</span>
       <button type="button">
         <IoAddCircleOutline
           size={30}
-          onClick={(e) => setCount(count == 10 ? 10 : count + 1)}
+          onClick={(e) => onQuantityChanged(quantity == 10 ? 10 : quantity + 1)}
         />
       </button>
     </div>
