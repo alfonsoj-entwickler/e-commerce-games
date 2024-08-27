6fe7383c390f06f92170244ba6f325bd48386336
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-08-27 08:46:14 +0200

    [TASK] get a game by slug

---
+++ src/actions/game/get-game-by-slug.ts
@@ -0,0 +1,31 @@
+'use server'
+
+import prisma from "@/lib/prisma";
+
+export const getGameBySlug = async ( slug:string) => {
+    try {
+        const game = await prisma.game.findFirst({
+            include: {
+                GameImage: {
+                    select: {
+                        url: true
+                    }
+                }
+            },
+            where: {
+                slug: slug,
+            }
+        });
+        
+        if (!game) return null;
+
+        return {
+            ...game,
+            images: game.GameImage.map(image => image.url)
+        };
+        
+    } catch (error) {
+        console.log(error);
+        throw new Error('Game not found')
+    }
+}
