961b3ad1e8c450ead528be0dbc30251ab4fc0b9b
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-08-30 19:22:47 +0200

    [TASK] added games item from db

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,25 +1,37 @@
-import { GameGrid, Title } from "@/components";
-import { notFound } from "next/navigation";
-import { initialData, ValidHardwares } from "@/seed/seed";
+export const revalidate = 60;
 
-const games = initialData.products;
+import { redirect } from "next/navigation";
+import { GameGrid, Pagination, Title } from "@/components";
+import { getPagintionGameWithImages } from "@/actions";
+import { Hardware } from "@prisma/client";
 
 interface Props {
+  searchParams: {
+    page?: string;
+  };
   params: {
-    id: ValidHardwares;
+    id: string;
   };
 }
 
-export default function Category({ params }: Props) {
+export default async function Category({ params, searchParams }: Props) {
   const { id } = params;
-  //if (id === "nintendo") {
-  //  notFound();
-  //}
+  const page = searchParams.page ? parseInt(searchParams.page) : 1;
+
+  const { games, currentPage, totalPages } = await getPagintionGameWithImages({
+    page: Number(page),
+    category: id as Hardware,
+  });
+
+  if (games.length === 0) {
+    redirect(`/category/${id}`);
+  }
 
   return (
     <>
       <Title title={id.toUpperCase()} subtitle="Games" className="mb-2" />
-      <GameGrid games={games.filter((item) => (item.hardware === id))} />
+      <GameGrid games={games} />
+      <Pagination totalPages={totalPages} />
     </>
   );
 }
