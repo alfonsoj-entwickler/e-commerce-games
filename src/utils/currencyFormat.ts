35d39add1f7e70600d3377d74931b2b458a9c3dc
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-09-27 18:11:14 +0200

    [TASK] created a function to format currency

---
+++ src/utils/currencyFormat.ts
@@ -0,0 +1,8 @@
+export const currencyFormat = (value: number) => {
+    return new Intl.NumberFormat('de-DE', {
+        style: 'currency',
+        currency: 'EUR',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    }).format(value);
+}
