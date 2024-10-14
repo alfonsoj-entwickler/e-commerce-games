f458eb22b264eec93af515b4f6850d3743881a32
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-10-14 21:53:50 +0200

    [TASK] created authenticate function

---
+++ src/actions/auth/login.ts
@@ -0,0 +1,15 @@
+'use server';
+
+import { signIn } from "@/auth.config";
+
+export async function authenticate( prevState: string | undefined, formData: FormData, ) {
+    try {
+        console.log("formData --> ", Object.fromEntries(formData));
+        await signIn('credentials', { ...Object.fromEntries(formData), redirect: false,});
+        return 'Success';
+    } catch (error) {
+        console.log(error);
+
+        return 'CredentialsSignin'
+    }
+}
