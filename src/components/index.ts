d09b3ec6d9160146f4b3a49a6ec8c976c3e52252
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2025-01-09 18:52:47 +0100

    [TASK] added Provider component and updated index export

--- src/components/index.ts
+++ src/components/index.ts
@@ -11,3 +11,4 @@ export * from "./ui/top-menu/TopMenu";
 export * from "./ui/not-found/PageNotFound";
 export * from "./ui/title/Title";
 export * from "./ui/pagination/Pagination";
+export * from "./provider/Provider";
