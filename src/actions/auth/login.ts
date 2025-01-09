9e1c99cd5e65e45f2de36e4f705c2bbf4ba82bc6
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2025-01-09 18:54:53 +0100

    [TASK] refactor authentication logic in login action and improve error handling

--- src/actions/auth/login.ts
+++ src/actions/auth/login.ts
@@ -4,12 +4,29 @@ import { signIn } from "@/auth.config";
 
 export async function authenticate( prevState: string | undefined, formData: FormData, ) {
     try {
-        console.log("formData --> ", Object.fromEntries(formData));
+        // console.log("formData --> ", Object.fromEntries(formData));
+        // await sleep(2);
+
         await signIn('credentials', { ...Object.fromEntries(formData), redirect: false,});
         return 'Success';
     } catch (error) {
-        console.log(error);
-
-        return 'CredentialsSignin'
+        console.log(error)
+        return 'UnknowError'
     }
+}
+
+export const login = async (email: string, password: string) => {
+    try {
+        await signIn('credentials', { email, password });
+        return {
+            ok: true
+        }
+    } catch (error) {
+        console.error(error)
+        return {
+            ok: false,
+            message: 'no session'
+        }
+    }
+    
 }
