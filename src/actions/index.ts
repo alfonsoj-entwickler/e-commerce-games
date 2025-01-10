024eaf2a3b9ba27c63f3f869a5e53fa06ac9d9ac
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2025-01-10 18:42:19 +0100

    [TASK] add user registration action and update index exports

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,3 +2,6 @@ export * from './game/game-pagination';
 export * from './game/get-game-by-slug';
 export * from './game/get-stock-by-slug';
 export * from './auth/login';
+export * from './auth/logout';
+export * from './auth/register';
+export * from './country/get-countries';
